5d442ad4c3631f711874ebadb18d50db5043dbd4
im0523 <hi_hoo2@naver.com>
2020-02-20 00:32:26 +0900

    commit test..

--- makeup/src/main/java/customer/CustomerVO.java
+++ makeup/src/main/java/customer/CustomerVO.java
@@ -2,8 +2,8 @@ package customer;
 
 public class CustomerVO {
 	private int no;
-	private String id, password, name, birthday, gender, email, address, hobby, graduate_h, schoolname_h, location_h;
-//			graduate_u, schoolname_u, location_u, graduate_m, schoolname_m, location_m;
+	private String id, password, name, birthday, gender, email, address, hobby, graduate_h, schoolname_h, location_h,
+			graduate_u, schoolname_u, location_u, graduate_m, schoolname_m, location_m;
 	
 	
 }
